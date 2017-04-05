56b98dfb65e7d65def1be3f8d82cf3705e0a0774
Eric Maranga <ekirira22@gmail.com>
2017-04-05 19:24:55 +0300

    Addition of a modal functions where we implement modal functionality in the contact route When a user enters an email and subsribes, it engenders a modal telling him/her congratulations!! :)

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  var user = {
+    email: this.get('email'),
+  };
 
+  // actions: {
+  //   pressed: function() {
+  //     Ember.$("#confirm").append("You made it!!")
+  //   }
+  // }
 });
